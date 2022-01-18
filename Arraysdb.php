7522e606a082f4a71a03791387c4e532781f5f99
JoseAngelRn <joseangel.ma44965@cesurformacion.com>
2022-01-18 10:14:01 +0100

    Arrays creados

--- Arraysdb.php
+++ Arraysdb.php
@@ -14,32 +14,57 @@ $array1 = array(
 );
 
 $array2  = array(
-    "Vueling-AE12" => "Boeing"
-    "Volotea-EA21" => "Airbus"
-    "Iberia-WE44" => "Airbus"
-    "Ryanair-EW44" => "Boeing"
-    "Vueling-ZK87" => "Airbus"
-    "Volotea-UA21" => "Boeing"
-    "Iberia-WH44" => "Boeing"
+    "Vueling-AE12" => "Boeing",
+    "Volotea-EA21" => "Airbus",
+    "Iberia-WE44" => "Airbus",
+    "Ryanair-EW44" => "Boeing",
+    "Vueling-ZK87" => "Airbus",
+    "Volotea-UA21" => "Boeing",
+    "Iberia-WH44" => "Boeing",
     "Ryanair-EL00" => "Airbus"
 );
 
 $array3 = array(
-    "" => ""
-    "" => ""
-    "" => ""
-    "" => ""
-    "" => ""
-    "" => ""
+    "Vueling-AE12" => "120 minutos",
+    "Volotea-EA21" => "100 minutos",
+    "Iberia-WE44" => "340 minutos",
+    "Ryanair-EW44" => "45 minutos",
+    "Vueling-ZK87" => "600 minutos",
+    "Volotea-UA21" => "65 minutos",
+    "Iberia-WH44" => "90 minutos",
+    "Ryanair-EL00" => "50 minutos",
+    "Vueling-AE12" => "115 minutos",
+    "Volotea-EA21" => "100 minutos",
+    "Iberia-WE44" => "85 minutos",
+    "Ryanair-EW44" => "360 minutos"
 );
 
 $array4 = array(
-    "" => ""
-    "" => ""
-    "" => ""
-    "" => ""
-    "" => ""
-    "" => ""
+    "Vueling-AE12" => "90 personas",
+    "Volotea-EA21" => "75 personas",
+    "Iberia-WE44" => "100 personas",
+    "Ryanair-EW44" => "60 personas",
+    "Vueling-ZK87" => "77 personas",
+    "Volotea-UA21" => "93 personas",
+    "Iberia-WH44" => "86 personas",
+    "Ryanair-EL00" => "75 personas",
+    "Vueling-AE12" => "87 personas",
+    "Volotea-EA21" => "97 personas",
+    "Iberia-WE44" => "81 personas",
+    "Ryanair-EW44" => "75 personas",
+    "Vueling-ZK87" => "87 personas",
+    "Volotea-UA21" => "90 personas",
+    "Iberia-WH44" => "84 personas",
+    "Ryanair-EL00" => "67 personas",
+    "Vueling-AE12" => "86 personas",
+    "Volotea-EA21" => "66 personas",
+    "Iberia-WE44" => "40 personas",
+    "Ryanair-EW44" => "77 personas",
+    "Vueling-ZK87" => "82 personas"
 );
 
+# print_r ($array1);
+# print_r ($array2);
+# print_r ($array3);
+# print_r ($array4);
 ?>
