797b8e0a823a06846048d1ca9bdbbf054268284e
JoseAngelRn <joseangel.ma44965@cesurformacion.com>
2022-01-18 10:26:26 +0100

    Empiezo con el switch

--- Mostrar_vuelo.php
+++ Mostrar_vuelo.php
@@ -5,5 +5,23 @@
 $text = $_POST["texto"];
 $select = $_POST["Mostrar_Vuelos"];
 
+#SWITCH DEL SELECT
 
+switch ($select) {
+    case 'Media_Pasajeros':
+        Media_Pasajeros($);
+        break;
+    case 'Ultimos_Destinos':
+        Ultimos_Destinos($);
+        break;
+    case 'Fabricante':
+        Fabricante($);
+        break;
+    case 'Minutos_Totales_Vuelo':
+        Minutos_Totales_Vuelo($);
+        break;
+    default:
+    echo "Elija otra opción";
+        break;
+}
 ?>
