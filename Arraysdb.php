20ed8092838274a3033445dae765eb1d7cfd6966
JoseAngelRn <joseangel.ma44965@cesurformacion.com>
2022-01-18 08:59:56 +0100

    Empiezo a crear la bd

--- Arraysdb.php
+++ Arraysdb.php
@@ -1,3 +1,38 @@
 <?php
 
+$array1 = array(
+    "Vueling-AE12" => "Milán",
+    "Volotea-EA21" => "Roma",
+    "Iberia-WE44" => "Estambul",
+    "Ryanair-EW44" => "Madrid",
+    "Vueling-ZK87" => "Lima",
+    "Volotea-UA21" => "Bilbao",
+    "Iberia-WH44" => "Roma",
+    "Ryanair-EL00" => "Madrid",
+    "Vueling-AE12" => "Roma",
+    "Volotea-EA21" => "Roma"
+);
+
+
+$array2  = array(
+    "Vueling-AE12" => ""
+    "Volotea-EA21" => ""
+    "Iberia-WE44" => ""
+    "Ryanair-EW44" => ""
+    "Vueling-ZK87" => ""
+    "Volotea-UA21" => ""
+    "Iberia-WH44" => ""
+    "Ryanair-EL00" => ""
+);
+$ = array(
+    "" => ""
+    "" => ""
+    "" => ""
+    "" => ""
+    "" => ""
+    "" => ""
+);
+
+print_r ($array1);
+
 ?>
