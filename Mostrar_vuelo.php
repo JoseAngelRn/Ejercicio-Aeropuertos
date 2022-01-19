745237b4636c2789c662712af7dbfe457176b61b
JoseAngelRn <joseangel.ma44965@cesurformacion.com>
2022-01-19 08:36:46 +0100

    Cambio en las funciones de mostrar vuelos

--- Mostrar_vuelo.php
+++ Mostrar_vuelo.php
@@ -8,20 +8,56 @@ $select = $_POST["Mostrar_Vuelos"];
 #SWITCH DEL SELECT
 
 switch ($select) {
-    case 'Media_Pasajeros':
+    case 'Vueling-AE12':
         Media_Pasajeros($text);
-        break;
-    case 'Ultimos_Destinos':
         Ultimos_Destinos($text);
-        break;
-    case 'Fabricante':
         Fabricante($text);
+        Minutos_Totales_Vuelo($text);
         break;
-    case 'Minutos_Totales_Vuelo':
+    case 'Volotea-EA21':
+        Media_Pasajeros($text);
+        Ultimos_Destinos($text);
+        Fabricante($text);
+        Minutos_Totales_Vuelo($text);
+        break;
+    case 'Iberia-WE44':
+        Media_Pasajeros($text);
+        Ultimos_Destinos($text);
+        Fabricante($text);
+        Minutos_Totales_Vuelo($text);
+        break;
+    case 'Ryanair-EW44':
+        Media_Pasajeros($text);
+        Ultimos_Destinos($text);
+        Fabricante($text);
+        Minutos_Totales_Vuelo($text);
+        break;
+    case 'Vueling-ZK87':
+        Media_Pasajeros($text);
+        Ultimos_Destinos($text);
+        Fabricante($text);
+        Minutos_Totales_Vuelo($text);
+        break;
+    case 'Volotea-UA21':
+        Media_Pasajeros($text);
+        Ultimos_Destinos($text);
+        Fabricante($text);
+        Minutos_Totales_Vuelo($text);
+        break;
+    case 'Iberia-WH44':
+        Media_Pasajeros($text);
+        Ultimos_Destinos($text);
+        Fabricante($text);
+        Minutos_Totales_Vuelo($text);
+        break;
+    case 'Ryanair-EL00':
+        Media_Pasajeros($text);
+        Ultimos_Destinos($text);
+        Fabricante($text);
         Minutos_Totales_Vuelo($text);
         break;
     default:
-    echo "Elija otra opción";
+        echo "Elija otra opción";
         break;
 }
 
