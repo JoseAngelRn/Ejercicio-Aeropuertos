242521a0c7badad2cec7ad7bc48701502358ef08
JoseAngelRn <joseangel.ma44965@cesurformacion.com>
2022-01-19 08:32:20 +0100

    Arreglo del select de mostrar vuelos

--- Funciones_Aeropuerto.php
+++ Funciones_Aeropuerto.php
@@ -5,26 +5,26 @@ $text = "Iberia-WE44";
 $EJ = 0;
 $array4 = array(
     array("Vuelos" => "Vueling-AE12", "Pasajeros" => 90),
-    array("Vuelos" => "Volotea-EA21"  , "Pasajeros" => 75),
+    array("Vuelos" => "Volotea-EA21", "Pasajeros" => 75),
     array("Vuelos" => "Iberia-WE44", "Pasajeros" => 100),
-    array("Vuelos" => "Ryanair-EW44" , "Pasajeros" => 60),
-    array("Vuelos" => "Vueling-ZK87"  , "Pasajeros" => 77),
-    array("Vuelos" => "Volotea-UA21"  , "Pasajeros" => 93),
-    array("Vuelos" => "Iberia-WH44"  , "Pasajeros" => 86),
-    array("Vuelos" => "Ryanair-EL00" , "Pasajeros" => 75),
-    array("Vuelos" => "Vueling-AE12"  , "Pasajeros" => 87),
-    array("Vuelos" => "Volotea-EA21"  , "Pasajeros" => 97),
-    array("Vuelos" => "Iberia-WE44" , "Pasajeros" => 81),
-    array("Vuelos" => "Ryanair-EW44" , "Pasajeros" => 75),
-    array("Vuelos" => "Vueling-ZK87"  , "Pasajeros" => 87),
-    array("Vuelos" => "Volotea-UA21" , "Pasajeros" => 90),
-    array("Vuelos" => "Iberia-WH44"  , "Pasajeros" => 84),
-    array("Vuelos" => "Ryanair-EL00" , "Pasajeros" => 67),
-    array("Vuelos" => "Vueling-AE12" , "Pasajeros" => 86),
-    array("Vuelos" => "Volotea-EA21"  , "Pasajeros" => 66),
-    array("Vuelos" => "Iberia-WE44" , "Pasajeros" => 40),
-    array("Vuelos" => "Ryanair-EW44"  , "Pasajeros" => 77),
-    array("Vuelos" => "Vueling-ZK87" , "Pasajeros" => 82)
+    array("Vuelos" => "Ryanair-EW44", "Pasajeros" => 60),
+    array("Vuelos" => "Vueling-ZK87", "Pasajeros" => 77),
+    array("Vuelos" => "Volotea-UA21", "Pasajeros" => 93),
+    array("Vuelos" => "Iberia-WH44", "Pasajeros" => 86),
+    array("Vuelos" => "Ryanair-EL00", "Pasajeros" => 75),
+    array("Vuelos" => "Vueling-AE12" , "Pasajeros" => 87),
+    array("Vuelos" => "Volotea-EA21", "Pasajeros" => 97),
+    array("Vuelos" => "Iberia-WE44", "Pasajeros" => 81),
+    array("Vuelos" => "Ryanair-EW44", "Pasajeros" => 75),
+    array("Vuelos" => "Vueling-ZK87", "Pasajeros" => 87),
+    array("Vuelos" => "Volotea-UA21", "Pasajeros" => 90),
+    array("Vuelos" => "Iberia-WH44", "Pasajeros" => 84),
+    array("Vuelos" => "Ryanair-EL00", "Pasajeros" => 67),
+    array("Vuelos" => "Vueling-AE12", "Pasajeros" => 86),
+    array("Vuelos" => "Volotea-EA21", "Pasajeros" => 66),
+    array("Vuelos" => "Iberia-WE44", "Pasajeros" => 40),
+    array("Vuelos" => "Ryanair-EW44", "Pasajeros" => 77),
+    array("Vuelos" => "Vueling-ZK87", "Pasajeros" => 82)
 );
 
 foreach ($array4 as $array_4) {
@@ -37,7 +37,6 @@ if ($array_4["Vuelos"] == $text) {
     $res = $EJ;
     }
 }
-
 echo $res;
 
 
