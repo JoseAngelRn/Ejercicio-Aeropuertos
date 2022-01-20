0bfc11fbb9bfaebe8b342208e392b68dab68d2a5
Jose Angel <jjoseaangel99@gmail.com>
2022-01-20 18:09:55 +0100

    Mas fuinciones

--- Estadistica_Avion.php
+++ Estadistica_Avion.php
@@ -1,6 +1,63 @@
 <?php
-INCLUDE_ONCE 'Funciones_Estadistica_Avion.php';
+INCLUDE_ONCE 'Funciones_Estadisticas_Avion.php';
 
+#RECOGIDA VARIABLES
+$select = $_POST["Estadisticas_avion"];
+$text = $_POST["text"];
 
+#SWITCH DEL SELECT
+switch ($select) {
+    case 'Vueling-AE12':
+        Destinos_Visitados($text, $array1);
+        Fabricante($text, $array2);
+        Minutos_Totales_Vuelo($text, $array3, $EJ);
+        Numero_Total_Pasajeros($array4, $text, $EJ);
+        break;
+    case 'Volotea-EA21':
+        Destinos_Visitados($text, $array1);
+        Fabricante($text, $array2);
+        Minutos_Totales_Vuelo($text, $array3, $EJ);
+        Numero_Total_Pasajeros($array4, $text, $EJ);
+        break;
+    case 'Iberia-WE44':
+        Destinos_Visitados($text, $array1);
+        Fabricante($text, $array2);
+        Minutos_Totales_Vuelo($text, $array3, $EJ);
+        Numero_Total_Pasajeros($array4, $text, $EJ);
+        break;
+    case 'Ryanair-EW44':
+        Destinos_Visitados($text, $array1);
+        Fabricante($text, $array2);
+        Minutos_Totales_Vuelo($text, $array3, $EJ);
+        Numero_Total_Pasajeros($array4, $text, $EJ);
+        break;
+    case 'Vueling-ZK87':
+        Destinos_Visitados($text, $array1);
+        Fabricante($text, $array2);
+        Minutos_Totales_Vuelo($text, $array3, $EJ);
+        Numero_Total_Pasajeros($array4, $text, $EJ);
+        break;
+    case 'Volotea-UA21':
+        Destinos_Visitados($text, $array1);
+        Fabricante($text, $array2);
+        Minutos_Totales_Vuelo($text, $array3, $EJ);
+        Numero_Total_Pasajeros($array4, $text, $EJ);
+        break;
+    case 'Iberia-WH44':
+        Destinos_Visitados($text, $array1);
+        Fabricante($text, $array2);
+        Minutos_Totales_Vuelo($text, $array3, $EJ);
+        Numero_Total_Pasajeros($array4, $text, $EJ);
+        break;
+    case 'Ryanair-EL00':
+        Destinos_Visitados($text, $array1);
+        Fabricante($text, $array2);
+        Minutos_Totales_Vuelo($text, $array3, $EJ);
+        Numero_Total_Pasajeros($array4, $text, $EJ);
+        break;
+    default:
+        echo "Elija otra opción";
+        break;
+}
 
 ?>
--- Funciones_Estadisticas_Avion.php
+++ Funciones_Estadisticas_Avion.php
@@ -2,16 +2,45 @@
 INCLUDE_ONCE 'Arraysdb.php';
 INCLUDE_ONCE 'Estadistica_Avion.php';
 
-
+$EJ=0;
 
 #FUNCIÓN DESTINOS DEL AVIÓN---------------------------------------------------------------------------
-
+function Destinos_Visitados($text, $array1){
+    foreach ($array1 as $array_1) {
+        $Destino = $array_1["Destino"];
+        $Vuelos = $array_1["Vuelo"];
+    
+    if ($Vuelos == $text) {
+        echo "Destinos: ".$Destino."<br>";
+        }
+    }
+    }
 
 #FUNCIÓN FABRICANTE DEL AVIÓN---------------------------------------------------------------------------
-
+function Fabricante($text, $array2){
+    foreach ($array2 as $array_2) {
+        $Fabricante = $array_2["Fabricante"];
+        $Vuelos = $array_2["Vuelo"];
+    
+    if ($Vuelos == $text) {
+        echo "El fabricante es: ".$Fabricante."<br>";
+        }
+    }
+    }
 
 #FUNCIÓN MINUTOS TOTALES VOLADOS---------------------------------------------------------------------------
-
+function Minutos_Totales_Vuelo($text, $array3, $EJ){
+    foreach ($array3 as $array_3) {
+        $Duracion_Minutos = $array_3["Duracion_Minutos"];
+        $Vuelos = $array_3["Vuelo"];
+    
+    if ($Vuelos == $text) {
+        $EJ = $EJ + $Duracion_Minutos;
+        $res = $EJ;
+        }
+    }
+    echo "Los minutos totales de vuelo han sido: ".$res."<br>";
+    }
 
 #FUNCIÓN MEDIA DE HORAS VOLADAS---------------------------------------------------------------------------
 
@@ -20,6 +49,17 @@ INCLUDE_ONCE 'Estadistica_Avion.php';
 
 
 #FUNCIÓN PASAJEROS TOTALES---------------------------------------------------------------------------
-
+function Numero_Total_Pasajeros($array4, $text, $EJ){
+    foreach ($array4 as $array_4) {
+        $Pasajeros = $array_4["Pasajeros"];
+        $Vuelos = $array_4["Vuelo"];
+        
+    if ($Vuelos == $text) {
+        $EJ = $EJ + $Pasajeros;
+        $res = $EJ;
+        }
+    }
+    echo "El número total de pasajeros del avion: ".$res."<br>";
+}
 
 ?>
