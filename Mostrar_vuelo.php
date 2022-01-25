5e29d92650b2c1d697319453c0c9712174cda56e
JoseAngelRn <joseangel.ma44965@cesurformacion.com>
2022-01-25 12:54:29 +0100

    color

--- Mostrar_vuelo.php
+++ Mostrar_vuelo.php
@@ -4,6 +4,7 @@ INCLUDE 'Funciones_Aeropuerto.php';
 #RECOGIDA DE VARIABLES
 $text = $_POST["Mostrar_Vuelos"];
 
+
 #REPRESENTACIÓN
 echo "<h2 style='color:FF5733;'>".$text."</h2>";
 Media_Pasajeros($text, $array4, $EJ);
